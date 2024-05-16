8c13650ea8c1c9a8617f7fec868491dd97448b75
Nexenek <malpy634@gmail.com>
2024-05-16 10:42:25 +0200

    W portalu ogloszeniowym dodano skrypt do liczenia cen ogloszen

---
+++ code/witryny/portalogloszeniowy/kalkulacja.js
@@ -0,0 +1,18 @@
+function kalkulacja() {
+    var ogloszenia = document.getElementById("ogloszenia").value;
+    var staly = document.getElementById("staly").checked;
+    var wynik = document.getElementById("wynik");
+    var koszt = 0;
+
+    if (ogloszenia > 40) {
+        koszt = parseInt(ogloszenia) * 2;
+    } else {
+        koszt = parseInt(ogloszenia) * 3;
+    }
+
+    if (staly) {
+        koszt = parseFloat(koszt) - 0.3;
+    }
+
+    wynik.innerHTML = "Twoje ogłoszenia będą kosztować: " + koszt + " PLN";
+}
