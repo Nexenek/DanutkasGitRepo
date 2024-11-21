0c3e4203f211ceba847243b0e0dff430b357174a
Nexenek <malpy634@gmail.com>
2024-11-21 09:36:29 +0100

    Progress bar działa

--- code/witryny/zarejestruj/skrypt.js
+++ code/witryny/zarejestruj/skrypt.js
@@ -28,3 +28,19 @@ function kontakt() {
 function zatwierdz() {
     // pass
 }
+
+let postepWartosc = 0;
+function aktualizujPostep() {
+    if (postepWartosc >= 100) {
+        postepWartosc = 100;
+    } else {
+        postepWartosc += 12;
+    }
+    document.querySelector("#postep>div").style.width = postepWartosc + "%";
+
+
+}
+
+document.querySelectorAll('input[type="text"], input[type="date"], input[type="number"], input[type="tel"]').forEach(input => {
+    input.addEventListener('blur', aktualizujPostep);
+})
