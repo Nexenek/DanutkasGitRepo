84463c8fff5b854458a6e7ba826e0882e03285b4
nexenek <malpy634@gmail.com>
2025-09-16 13:37:25 +0200

    motywy koniec 16.09.2025 13:37

--- code/witryny/motywy/script.js
+++ code/witryny/motywy/script.js
@@ -3,4 +3,29 @@ function zastosujEfekt() {
     const efekty = document.querySelectorAll('input[name="opcja"]');
     efekty.forEach((efekt) => {
         if (efekt.checked) {
+            zdjecie.style.filter = efekt.value;
+        }
+    });
+}
+
+function kolor() {
+    const zdjecie = document.querySelector('img[src="pomarancza.jpg"]');
+    zdjecie.style.filter = `none`;
+}
+
+function czarnoBialy() {
+    const zdjecie = document.querySelector('img[src="pomarancza.jpg"]');
+    zdjecie.style.filter = 'grayscale(100%)';
+}
+
+function zastosujPrzezroczystosc() {
+    const zdjecie = document.querySelector('img[src="owoce.jpg"]');
+    const przezroczystosc = document.getElementById('suwak3').value;
+    zdjecie.style.filter = `opacity(${przezroczystosc}%)`;
+}
+
+function zastosujJasnosc() {
+    const zdjecie = document.querySelector('img[src="zolw.jpg"]');
+    const jasnosc = document.getElementById('suwak4').value;
+    zdjecie.style.filter = `brightness(${jasnosc}%)`;
 }
