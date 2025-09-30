4a669018808fb9e9826f82fd5a59b3f656f69190
nexenek <malpy634@gmail.com>
2025-09-30 13:20:37 +0200

    case ver galeria_obrazow

--- code/witryny/galeria_obrazow/script.js
+++ code/witryny/galeria_obrazow/script.js
@@ -1,20 +1,93 @@
 let numer_zdj = 1;
 
+// function pokaz(nazwa_pliku) {
+//     document.getElementById("duze").src = nazwa_pliku;
+//     numer_zdj = parseInt(nazwa_pliku);
+// }
+
 function pokaz(nazwa_pliku) {
-    document.getElementById("duze").src = nazwa_pliku;
-    numer_zdj = parseInt(nazwa_pliku);
+    switch (nazwa_pliku) {
+        case "1.jpg":
+            document.getElementById("duze").src = "1.jpg";
+            numer_zdj = 1;
+            break;
+        case "2.jpg":
+            document.getElementById("duze").src = "2.jpg";
+            numer_zdj = 2;
+            break;
+        case "3.jpg":
+            document.getElementById("duze").src = "3.jpg";
+            numer_zdj = 3;
+            break;
+        case "4.jpg":
+            document.getElementById("duze").src = "4.jpg";
+            numer_zdj = 4;
+            break;
+        case "5.jpg":
+            document.getElementById("duze").src = "5.jpg";
+            numer_zdj = 5;
+            break;
+        default:
+            break;
+    }
 }
 
 function poprzednie() {
-    if (numer_zdj > 1) {
-        numer_zdj--;
-        document.getElementById("duze").src = numer_zdj + ".jpg";
+    switch (numer_zdj) {
+        case 2:
+            numer_zdj--;
+            document.getElementById("duze").src = "1.jpg";
+            break;
+        case 3:
+            numer_zdj--;
+            document.getElementById("duze").src = "2.jpg";
+            break;
+        case 4:
+            numer_zdj--;
+            document.getElementById("duze").src = "3.jpg";
+            break;
+        case 5:
+            numer_zdj--;
+            document.getElementById("duze").src = "4.jpg";
+            break;
+        default:
+            break;
     }
 }
 
 function nastepne() {
-    if (numer_zdj < 5) {
-        numer_zdj++;
-        document.getElementById("duze").src = numer_zdj + ".jpg";
+    switch (numer_zdj) {
+        case 1:
+            numer_zdj++;
+            document.getElementById("duze").src = "2.jpg";
+            break;
+        case 2:
+            numer_zdj++;
+            document.getElementById("duze").src = "3.jpg";
+            break;
+        case 3:
+            numer_zdj++;
+            document.getElementById("duze").src = "4.jpg";
+            break;
+        case 4:
+            numer_zdj++;
+            document.getElementById("duze").src = "5.jpg";
+            break;
+        default:
+            break;
     }
 }
+
+// function poprzednie() {
+//     if (numer_zdj > 1) {
+//         numer_zdj--;
+//         document.getElementById("duze").src = numer_zdj + ".jpg";
+//     }
+// }
+// 
+// function nastepne() {
+//     if (numer_zdj < 5) {
+//         numer_zdj++;
+//         document.getElementById("duze").src = numer_zdj + ".jpg";
+//     }
+// }
