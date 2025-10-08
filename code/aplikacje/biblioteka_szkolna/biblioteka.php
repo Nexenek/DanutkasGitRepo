857d1ed6e59866caec3017740d677f0a13e82afa
nexenek <malpy634@gmail.com>
2025-10-08 07:54:56 +0200

    nie mam juz pomyslu co tu pisac

--- code/aplikacje/biblioteka_szkolna/biblioteka.php
+++ code/aplikacje/biblioteka_szkolna/biblioteka.php
@@ -29,40 +29,24 @@
         echo '<td>'.htmlspecialchars($wiersz[2]).'</td>';
         echo '</tr>';
       }
-      mysqli_free_result($wynik);
       mysqli_close($polaczenie);
       ?>
     </table>
   </section>
 
   <main>
-    <?php
-    // Wczytaj streszczenia z pliku teksty.txt
-    $teksty = file_get_contents('teksty.txt');
-    $paragrafy = preg_split('/\R{2,}/', trim($teksty));
-    // Zakładamy, że zawiera tytuły i streszczenia w znanym porządku.
-    $summaries = array_slice($paragrafy, 2, 3);
-    $images = ['ksiazka1.jpg','ksiazka2.jpg','ksiazka3.jpg'];
-    $captions = [
-      "HANS CHRISTIAN ANDERSEN<br><strong>Ropucha</strong>",
-      "MARIA KONOPNICKA<br><strong>Przygoda z lalką</strong>",
-      "IGNACY KRASICKI<br><strong>Przyjaciel</strong>",
-    ];
-
-    for ($i=0;$i<3;$i++) {
-      echo '<article>';
-      // obrazki w repo są w formacie png; wymaganie egzaminacyjne mówi jpg,
-      // przy braku jpg używamy dostępnych png (użytkownik może przekopiować jako jpg lokalnie).
-      $src = $images[$i];
-      echo '<figure>';
-      echo '<img src="'.htmlspecialchars($src).'" alt="okładka książki">';
-      echo '</figure>';
-      echo '<div class="title-box" style="font-family: serif;">'. $captions[$i] .'</div>';
-      $text = isset($summaries[$i]) ? nl2br(htmlspecialchars($summaries[$i])) : '';
-      echo '<p>'.$text.'</p>';
-      echo '</article>';
-    }
-    ?>
+    <article id="niebieski">
+      <img src="ksiazka1.jpg" alt="okładka książki">
+      <p>Według różnych podań najpaskudniejsza ropucha nosi w głowie piękny, cenny klejnot.</p>
+    </article>
+    <article id="zielony">
+      <img src="ksiazka2.jpg" alt="okładka książki">
+      <p>Panna Stefcia i Maryla nie są to zbyt grzeczne damy, nawet nie słuchają mamy...</p>
+    </article>
+    <article id="fioletowy">
+      <img src="ksiazka3.jpg" alt="okładka książki">
+      <p>Ratuj mnie, przyjacielu, w ostatniej potrzebie: Kocham piękną Irenę. Rodzice i ona...</p>
+    </article>
   </main>
 
   <footer>
