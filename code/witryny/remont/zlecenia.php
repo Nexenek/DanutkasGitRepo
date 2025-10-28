84849fa7f31b13a1e6b9f79f866f547b52b2e0f2
nexenek <malpy634@gmail.com>
2025-10-28 13:39:59 +0100

    zlecenia.php skrypt 2 i 3

--- code/witryny/remont/zlecenia.php
+++ code/witryny/remont/zlecenia.php
@@ -32,7 +32,7 @@
                         $zap2 = "SELECT `nazwa_firmy`, `liczba_pracownikow` FROM `wykonawcy` WHERE liczba_pracownikow>=$licz_pracownik;";
                         $wynik2 = mysqli_query($link, $zap2);
                         while ($wiersz = mysqli_fetch_array($wynik2)) {
-                            echo"<p>$wiersz[0], $wiersz[1]</p>";
+                            echo"<p>$wiersz[0], $wiersz[1] pracowników</p>";
                         }
                     }
                 ?>
@@ -41,12 +41,36 @@
         <section id="srodek">
             <h2>Dla wykonawców</h2>
             <form action="zlecenia.php" name="wykonawcy" method="post">
-                <select>
-                    <?php
-                        //skrypt 2
-                    ?>
-                </select>
+            <select name="miasto">
+                <?php
+                $link = mysqli_connect('localhost', 'root', '', 'remonty');
+                $zap3 = "SELECT DISTINCT miasto FROM klienci ORDER BY miasto ASC;";
+                $wynik3 = mysqli_query($link, $zap3);
+                while ($wiersz = mysqli_fetch_array($wynik3)) {
+                    echo "<option value='$wiersz[0]'>$wiersz[0]</option>";
+                }
+                ?>
+            </select><br>
+            <input type="radio" id="malowanie" name="usluga" value="malowanie" checked>
+            <label for="malowanie">malowanie</label><br>
+            <input type="radio" id="gipsowanie" name="usluga" value="gipsowanie">
+            <label for="gipsowanie">gipsowanie</label><br>
+            <input type="submit" name="szukaj_klientow" value="Szukaj klientów">
             </form>
+            <?php
+            if (isset($_POST['szukaj_klientow'])) {
+                $miasto = $_POST['miasto'] ?? '';
+                $usluga = $_POST['usluga'] ?? '';
+                $link = mysqli_connect('localhost', 'root', '', 'remonty');
+                $zap4 = "SELECT k.imie, z.cena FROM klienci k, zlecenia z WHERE z.id_klienta = k.id_klienta and k.miasto='$miasto' and z.rodzaj='$usluga';";
+                $wynik4 = mysqli_query($link, $zap4);
+                echo "<ul>";
+                while ($wiersz = mysqli_fetch_array($wynik4)) {
+                echo "<li>$wiersz[0] - $wiersz[1]</li>";
+                }
+                echo "</ul>";
+            }
+            ?>
         </section>
     </main>
     <footer>
