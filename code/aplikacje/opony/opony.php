eed5acaad65505d3e046de8eea3dd356800ef6f5
nexenek <malpy634@gmail.com>
2025-10-30 12:04:41 +0100

    opony

---
+++ code/aplikacje/opony/opony.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>OPONY</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>OPONY</h1>
+    </header>
+    <main>
+        <aside>
+            <?php
+            // Skrypt 1
+            ?>
+            <p><a href="https://opona.pl/">więcej ofert</a></p>
+        </aside>
+        <section>
+            <img src="opona.png" alt="Opona">
+            <h2>Opona dnia</h2>
+            <?php
+            // Skrypt 2
+            ?>
+        </section>
+        <section>
+            <h2>Najnowsze zamówienie</h2>
+            <?php
+            // Skrypt 3
+            ?>
+        </section>
+    </main>
+    <footer>
+        <p>Stronę wykonał: 123456</p>
+    </footer>
+</body>
+</html>
