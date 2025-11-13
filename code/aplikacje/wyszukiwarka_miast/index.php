85dcfcfc6d0a0715725891702c760d85a8d49564
nexenek <malpy634@gmail.com>
2025-11-13 12:09:39 +0100

    mysqli

--- code/aplikacje/wyszukiwarka_miast/index.php
+++ code/aplikacje/wyszukiwarka_miast/index.php
@@ -23,21 +23,20 @@
             <div id="prawy">
                 <h1>Wyniki wyszukiwania miast z uwzględnieniem filtra:</h1>
                 <?php
-                    // Skrypt
                     if (isset($_POST['szukaj'])) {
                         $miasto = $_POST['miasto'];
                         echo "<p>$miasto</p>";
 
-                        $conn = new mysqli(hostname: "localhost",username: "root",password: "",database: "wykaz");
+                        $conn = mysqli_connect("localhost", "root", "", "wykaz");
                         $sql = "SELECT miasta.nazwa AS miasta_nazwa, wojewodztwa.nazwa AS wojewodztwa_nazwa FROM miasta JOIN wojewodztwa ON wojewodztwa.id = id_wojewodztwa WHERE miasta.nazwa LIKE '$miasto%' ORDER BY miasta.nazwa;";
-                        $result = $conn->query($sql);
+                        $result = mysqli_query($conn, $sql);
 
                         echo "<table>";
                             echo "<tr>";
                                 echo "<th>Miasto</th>";
                                 echo "<th>Województwo</th>";
                             echo "</tr>";
-                        while ($row = $result->fetch_assoc()) {
+                        while ($row = mysqli_fetch_array($result)) {
                             echo "<tr>";
                                 echo "<td>".$row['miasta_nazwa']."</td>";
                                 echo "<td>".$row['wojewodztwa_nazwa']."</td>";
@@ -45,7 +44,7 @@
                         }
                         echo "</table>";
 
-                        $conn -> close();
+                        mysqli_close($conn);
                     }
 
                 ?>
