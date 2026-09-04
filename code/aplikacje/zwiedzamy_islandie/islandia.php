58a9184d9102f4d5aa567beba94b71c6f3bff578
Nexenek <malpy634@gmail.com>
2026-09-04 12:44:57 +0200

    isladnia

---
+++ code/aplikacje/zwiedzamy_islandie/islandia.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="PL-pl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Isladnia</title>
+        <link rel="stylesheet" type="text/css" href="styl.css">
+    </head>
+<body>
+
+</body>
+</html>
