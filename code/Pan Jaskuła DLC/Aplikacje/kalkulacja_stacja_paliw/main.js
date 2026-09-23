1e22f20d51749305b33250213b699f74e8e41cb1
nexenek <malpy634@gmail.com>
2026-09-23 12:46:22 +0200

    koniec lekcji

--- code/Pan Jaskuła DLC/Aplikacje/kalkulacja_stacja_paliw/main.js
+++ code/Pan Jaskuła DLC/Aplikacje/kalkulacja_stacja_paliw/main.js
@@ -2,5 +2,20 @@ function oblicz() {
     let dystans = document.getElementById("dystans").value;
     let spalanie = document.getElementById("spalanie").value;
     const wynik = document.getElementById("wynik");
-    let paliwo = (dystans / spalanie) * 100;
+    let typ = document.getElementById("typ").value;
+    
+    let paliwo = ((dystans / 100) * spalanie).toFixed(2);
+    let platnosc = 0
+
+    if (typ == "benzyna") {
+        let cena = 7.99;
+        platnosc = (paliwo * cena).toFixed(2);
+    } else if (typ == "disel") {
+        let cena = 9.23;
+        platnosc = (paliwo * cena).toFixed(2);
+    } else {
+        console.log("error")
+    }
+
+    wynik.innerHTML = "Potrzebujesz " + paliwo + " litrów paliwa.\nZapłacisz za to " + platnosc + "PLN."
 }
